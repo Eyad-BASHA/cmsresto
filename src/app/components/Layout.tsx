9e7ce953d80451ac30e0868b805f305a2f9c7f99
Eyad BASHA <eyad.esgi@gmail.com>
2024-08-26 23:32:48 +0200

    use client dans layout

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,21 +1,21 @@
-
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Navbar from './Navbar';
-import { UserProvider } from '@/context/UserContext';
+"use client";
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import { UserProvider } from "@/context/UserContext";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return (
-        <UserProvider>
-            <div className="layout w-full">
-                <Header />
-                {/* <Navbar /> */}
-                <main>{children}</main>
-                <Footer />
-            </div>
-        </UserProvider>
-    );
+  return (
+    <UserProvider>
+      <div className="layout w-full">
+        <Header />
+        {/* <Navbar /> */}
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </UserProvider>
+  );
 };
 
 export default Layout;
